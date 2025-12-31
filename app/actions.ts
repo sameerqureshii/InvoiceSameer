71bca74ea6c6150451c5ef0eb6207d50f0ac67c0
Sameer Ahmed Qureshi <sq56331@gmail.com>
2026-01-01 00:32:08 +0500

    deleted typescript plugin

--- app/actions.ts
+++ app/actions.ts
@@ -88,7 +88,9 @@ export async function createInvoice(prevState: any, formData: FormData){
                 amount: submission.value.total,
                 currency: submission.value.currency as any,
             }),
-            "invoiceLink": `http://localhost:3000/api/invoice/${data.id}`,
+            "invoiceLink": process.env.NODE_ENV !== 'production' 
+            ? `http://localhost:3000/api/invoice/${data.id}`
+            : `https://invoice-sameer00.vercel.app/api/invoice/${data.id}`,
         },
     });
 
@@ -149,7 +151,9 @@ export async function editInvoice(prevState: any, formData: FormData){
                 amount: submission.value.total,
                 currency: submission.value.currency as any,
             }),
-            "invoiceLink": `http://localhost:3000/api/invoice/${data.id}`,
+            "invoiceLink": process.env.NODE_ENV !== 'production' 
+            ? `http://localhost:3000/api/invoice/${data.id}`
+            : `https://invoice-sameer00.vercel.app/api/invoice/${data.id}`,
         },
     });
 
