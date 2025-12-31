818343899ec7e9a996c2aea33761895f9f605447
Sameer Ahmed Qureshi <sq56331@gmail.com>
2025-12-31 23:46:53 +0500

    deleted typescript plugin

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "prisma/config"
 
+if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is missing!")
+
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 })
