c02119311108724b81f4927402467a1bbd30645a
Sameer Ahmed Qureshi <sq56331@gmail.com>
2025-12-31 23:41:01 +0500

    deleted typescript plugin

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'prisma/config'
+import { defineConfig } from "prisma/config"
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
-    // shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL // optional
+    url: process.env.DATABASE_URL!,
   },
 })
